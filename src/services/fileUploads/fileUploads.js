707fd8ac8504e09abb0661d18ccd7d663ed3b321
AmiraReda2 <amiraredayousef@gmail.com>
2024-05-05 23:08:43 +0300

    first uploads

--- src/services/fileUploads/fileUploads.js
+++ src/services/fileUploads/fileUploads.js
@@ -7,7 +7,7 @@ import { AppError } from '../../utils/appError.js'
 export const fileUpload = () => {
     const storage = multer.diskStorage({
         destination: (req, file , cb)=>{
-            cb(null )
+            cb(null,'uploads/')
         },
         filename: (req, file ,cb) =>{
             cb(null , new mongoose.Types.ObjectId + "-" + file.originalname)
